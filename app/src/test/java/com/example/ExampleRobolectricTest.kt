bc87a4ba6cc4d6b747afd11bed6a99f0ab30e481
Amaan Syed <amaansyed27@gmail.com>
2026-08-03 02:23:36 +0530

    Replace template Robolectric test

--- app/src/test/java/com/example/ExampleRobolectricTest.kt
+++ app/src/test/java/com/example/ExampleRobolectricTest.kt
@@ -10,12 +10,10 @@ import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [36])
-class ExampleRobolectricTest {
-
-  @Test
-  fun `read string from context`() {
-    val context = ApplicationProvider.getApplicationContext<Context>()
-    val appName = context.getString(R.string.app_name)
-    assertEquals("My Application", appName)
-  }
+class AppIdentityTest {
+    @Test
+    fun appNameIsTakoFlow() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        assertEquals("TakoFlow", context.getString(R.string.app_name))
+    }
 }
