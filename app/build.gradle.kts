ec7339655284e010a1ee0df8b61d2903dd201b29
Amaan Syed <amaansyed27@gmail.com>
2026-08-07 07:26:55 +0530

    Wire ViewModel dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,6 +94,8 @@ dependencies {
   implementation(libs.androidx.datastore.preferences)
   implementation(libs.androidx.lifecycle.runtime.compose)
   implementation(libs.androidx.lifecycle.runtime.ktx)
+  implementation(libs.androidx.lifecycle.viewmodel.compose)
+  implementation(libs.androidx.lifecycle.viewmodel.ktx)
   implementation(libs.androidx.navigation.compose)
   implementation(libs.kotlinx.coroutines.android)
   implementation(libs.kotlinx.coroutines.core)
