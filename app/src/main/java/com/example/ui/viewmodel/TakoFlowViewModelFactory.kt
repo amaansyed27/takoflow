ee3e4443470e895e7274b3505b6bafbea7d55744
Amaan Syed <amaansyed27@gmail.com>
2026-08-07 07:29:20 +0530

    Add Compose ViewModel helper

--- app/src/main/java/com/example/ui/viewmodel/TakoFlowViewModelFactory.kt
+++ app/src/main/java/com/example/ui/viewmodel/TakoFlowViewModelFactory.kt
@@ -1,7 +1,10 @@
 package com.example.ui.viewmodel
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 class TakoFlowViewModelFactory<T : ViewModel>(
     private val creator: () -> T
@@ -14,4 +17,10 @@ class TakoFlowViewModelFactory<T : ViewModel>(
         }
         return viewModel as VM
     }
+}
+
+@Composable
+inline fun <reified VM : ViewModel> takoFlowViewModel(noinline creator: () -> VM): VM {
+    val factory = remember { TakoFlowViewModelFactory(creator) }
+    return viewModel(factory = factory)
 }
